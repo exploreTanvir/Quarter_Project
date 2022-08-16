4d74f06f93c606e75493c8d7655a9a01942e85a8
tanvirhossen853 <103171957+tanvirhossen853@users.noreply.github.com>
2022-08-17 01:35:26 +0600

    Quarter Project

---
+++ slick js/custom.js
@@ -0,0 +1,43 @@
+// $(".slider").slick({
+//   infinite: true,
+//   speed: 300,
+//   slidesToShow: 1,
+//   adaptiveHeight: true,
+//   arrows: true,
+//   autoplay: false,
+// });
+
+// $(".slider").slick({
+//   centerMode: true,
+//   centerPadding: "60px",
+//   slidesToShow: 3,
+//   responsive: [
+//     {
+//       breakpoint: 768,
+//       settings: {
+//         arrows: false,
+//         centerMode: true,
+//         centerPadding: "40px",
+//         slidesToShow: 3,
+//       },
+//     },
+//     {
+//       breakpoint: 480,
+//       settings: {
+//         arrows: false,
+//         centerMode: true,
+//         centerPadding: "40px",
+//         slidesToShow: 1,
+//       },
+//     },
+//   ],
+// });
+
+$(".single-item").slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  arrow: true,
+  cssEase: "linear",
+});
